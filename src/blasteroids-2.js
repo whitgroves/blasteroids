ef03f7f12af57729ada0f51513655efe33ed7338
whitgroves <whitney.groves@gmail.com>
2024-01-16 16:08:27 -0600

    reorg

--- src/blasteroids-2.js
+++ src/blasteroids-2.js
@@ -8,7 +8,7 @@ const canvas = document.getElementById('mainCanvas');
 const ctx = canvas.getContext('2d');
 
 const DEBUG = JSON.parse(document.getElementById('debugFlag').text).isDebug;
-const BUILD = '2024.01.16.0'; // makes it easier to check for cached version on mobile
+const BUILD = '2024.01.16.1'; // makes it easier to check for cached version on mobile
 
 // mobile settings
 const MOBILE = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent); // https://stackoverflow.com/a/29509267/3178898
